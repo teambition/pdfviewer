81ffb1878998a014746e59372187e9051722a458
zensh <admin@zensh.com>
2015-04-21 15:46:58 +0800

    v0.1.0

--- gulpfile.js
+++ gulpfile.js
@@ -1,14 +1,18 @@
 'use strict';
 
-var path = require('path'),
-  pack = require('./package'),
-  gulp = require('gulp'),
-  cssmin = require('gulp-cssmin'),
-  uglify = require('gulp-uglify'),
-  gulpSequence = require('gulp-sequence'),
-  jshint = require('gulp-jshint');
+var path = require('path');
+var gulp = require('gulp');
+var merge2 = require('merge2');
+var clean = require('gulp-rimraf');
+var uglify = require('gulp-uglify');
+var jshint = require('gulp-jshint');
+var minifyCss = require('gulp-minify-css');
+var gulpSequence = require('gulp-sequence');
 
-var dest = path.join('dist', pack.version)
+gulp.task('clean', function() {
+  return gulp.src('dist', {read: false})
+  .pipe(clean({force: true}));
+});
 
 gulp.task('jshint', function () {
   return gulp.src(['src/*.js', 'gulpfile.js', 'examples/*.js', 'test/*.js'])
@@ -16,24 +20,26 @@ gulp.task('jshint', function () {
     .pipe(jshint.reporter('default'));
 });
 
-gulp.task('copy', function(){
-  return gulp.src(['src/locale/**/*', 'src/css/images/*', 'src/js/pdf.js', 'src/js/pdf.worker.js'], {base: 'src'})
-    .pipe(gulp.dest(dest));
-})
-
-gulp.task('style', function(){
-  return gulp.src('src/css/*.css', {base: 'src'})
-    .pipe(cssmin())
-    .pipe(gulp.dest(dest));
+gulp.task('pdfjs', function(){
+  return merge2(
+    gulp.src('bower_components/pdf.js/build/generic/build/*.js'),
+    gulp.src([
+      'bower_components/pdf.js/build/generic/web/compatibility.js',
+      'bower_components/pdf.js/build/generic/web/l10n.js',
+      'src/js/viewer.js'
+    ])
+  ).pipe(uglify()).pipe(gulp.dest('dist/js'));
 });
 
-gulp.task('script', function(){
-  return gulp.src(['src/index.js', 'src/js/compatibility.js', 'src/js/viewer.js'], {base: 'src'})
-    .pipe(uglify())
-    .pipe(gulp.dest(dest));
+gulp.task('others', function(){
+  return merge2([
+    gulp.src(['src/locale/**', 'src/images/**'], {base: 'src'}),
+    gulp.src('src/css/*.css', {base: 'src'}).pipe(minifyCss()),
+    gulp.src('src/index.js' , {base: 'src'}).pipe(uglify())
+  ]).pipe(gulp.dest('dist'));
 });
 
-gulp.task('default', ['test']);
+gulp.task('default', ['build']);
 
 gulp.task('test', gulpSequence('jshint'));
-gulp.task('build', gulpSequence(['copy', 'style', 'script']))
+gulp.task('build', gulpSequence('clean', 'pdfjs', 'others'));
